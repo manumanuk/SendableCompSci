5ec01975567ee874183c1824139f15f5529d724d
aryankalia <kalia.aryan2013@gmail.com>
2020-06-20 02:05:13 -0400

    EO fix

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -9,33 +9,24 @@ import { LoginService } from '../services/login.service';
 /**
  * Controls View of Login Page and calls login service for authentication
  * @class
+ * @augments LoginService
  * @implements { OnInit }
  */
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent extends LoginService implements OnInit {
   /**
-   * Creates LoginPageComponent class and loads backend for login
-   * @constructor
-   * @param { LoginService } _loginService - Connects page view to backend tools for handling login events
+   * EXPLANATION FOR EXTENDING OBJECTS ASSIGNMENT
+   * When LoginPageComponent extends LoginService, it inherits all functions of LoginService, like login() and getLoginStatus()
+   * This way, a new function like signIn() or getLoginStatus() is not needed to call the LoginService methods, nor is a separate
+   * instance of LoginService required. This allows us to change the invocations of LoginPageComponent methods inside of
+   * login-page.component.html to directly reference the LoginService methods.
+   *
+   * Where is the instance of this function created?
+   * Angular automatically creates an instance of this function when the LoginPageComponent is loaded. When this occurs, the LoginService
+   * methods are also loaded (as part of LoginPageComponent) since LoginPageComponent extends the class
    */
-  constructor(private _loginService: LoginService) {}
 
   /**
    * When login page is loaded, checks if user has already logged in
    */
   ngOnInit(): void {}
-
-  /**
-  * Calls sign in method from backend LoginService object
-  */
-  signIn() {
-    this._loginService.login();
-  }
-
-  /**
-   * Retrieves login status
-   * @returns { boolean } True if logged in, false if user is not logged in
-   */
-  getLoginStatus() {
-    return this._loginService.isLoggedIn();
-  }
 }
